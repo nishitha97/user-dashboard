16105157fb1d5737ef714c576391385f34b18cdc
nishitha97 <nishithadesilva123@gmail.com>
2019-12-12 14:14:10 +0530

    first commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+import Index from "views/Index.jsx";
+import Profile from "views/examples/Profile.jsx";
+import Register from "views/examples/Register.jsx";
+import Login from "views/examples/Login.jsx";
+
+var routes = [
+    {
+        path: "/index",
+        name: "Dashboard",
+        icon: "ni ni-tv-2 text-primary",
+        component: Index,
+        layout: "/admin"
+    },
+
+    {
+        path: "/user-profile",
+        name: "User Profile",
+        icon: "ni ni-single-02 text-yellow",
+        component: Profile,
+        layout: "/admin"
+    },
+    {
+        path: "/login",
+        name: "Login",
+        icon: "ni ni-key-25 text-info",
+        component: Login,
+        layout: "/auth"
+    },
+    {
+        path: "/register",
+        name: "Register",
+        icon: "ni ni-circle-08 text-pink",
+        component: Register,
+        layout: "/auth"
+    }
+];
+export default routes;
